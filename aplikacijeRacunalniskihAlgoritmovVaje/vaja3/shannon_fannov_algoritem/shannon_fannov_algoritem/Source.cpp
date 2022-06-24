23730b473643dfc450f1af95e7fc581eeac83449
CabrianVid <108013878+CabrianVid@users.noreply.github.com>
2022-06-24 09:07:56 +0200

    Update Source.cpp

--- aplikacijeRacunalniskihAlgoritmovVaje/vaja3/shannon_fannov_algoritem/shannon_fannov_algoritem/Source.cpp
+++ aplikacijeRacunalniskihAlgoritmovVaje/vaja3/shannon_fannov_algoritem/shannon_fannov_algoritem/Source.cpp
@@ -1,32 +1,31 @@
-
 #include <iostream>
 #include <fstream>
-#include<vector>
-#include<cmath>
-#include <bitset>
+#include <utility>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-/*vector<vector<int>> codaZnaka(256);
-vector<int>steviloPonovitev(256);
-vector<int>zacetnaStevilka(256);
-*/
+struct Element {
+    unsigned int count;
+    char character;
+    vector<bool> codes;
 
-class Znak {
-public:
-    int zacetniZnak;
-    int steviloPonovitev;
-    vector<int> codaZnaka;
+    Element(unsigned int count, char character) : count(count), character(character) {}
 };
-
 
 class BinWriter {
 public:
     int k;
     ofstream f;
-    char x;
+    char x{};
 
-    BinWriter(std::string path) : k(0) {//path je datoteka
-        f.open(path, ios::binary);
+    int getK() const {
+        return k;
+    }
+
+    explicit BinWriter(const string &file) : k(0) {
+        f.open(file, ios::binary);
     }
 
     ~BinWriter() {
@@ -34,268 +33,291 @@ public:
         f.close();
     }
 
-    void writeByte(char x) {
-        f.write((char*)&x, 1);
+    void writeByte(char i) {
+        f.write((char *) &i, 1);
     }
 
     void writeInt(int y) {
-        f.write((char*)&y, 4);
+        f.write((char *) &y, 4);
     }
 
-    void writeFloat(float f1) {
-        f.write((char*)&f1, 4);
-    }
-
-    void writeBit(bool b) {//^ XOR
+    void writeBit(bool b) {
         if (k == 8) {
             writeByte(x);
             k = 0;
         }
-        x ^= (-b ^ x) & (1 << k);//nastavitev n-tega bita na b(0 ali 1)
+        x ^= (-b ^ x) & (1 << k);
         k++;
+    }
+
+    void writeFloat(float y) {
+        f.write((char *) &y, 4);
     }
 };
 
 class BinReader {
 public:
-    int k;
+    int bitCount;
     ifstream f;
-    char x;
-    int a;
-    float b;
+    char byte;
+    int integer;
+    float floating;
 
-    BinReader(std::string path) : k(0) {
-        f.open(path, ios::binary);
+    explicit BinReader(const string &file) : bitCount(0) {
+        f.open(file, ios::binary);
     }
 
     char readByte() {
-        f.read((char*)&x, sizeof(x));//lahko tudi 1
-        return x;
-    }
-
-    int readInt() {
-        f.read((char*)&a, 4);
-        return a;
-    }
-
-    float readFloat() {
-        f.read((char*)&b, sizeof(b));
-        return b;
+        f.read((char *) &byte, 1);
+        return byte;
     }
 
     bool readBit() {
-        if (k == 8) {
+        if (bitCount == 8) {
             readByte();
-            k = 0;
+            bitCount = 0;
         }
-        bool b = (x >> k) & 1;//branje k-tega bita
-        k++;
+        bool b = (byte >> bitCount) & 1;
+        bitCount++;
         return b;
+    }
+
+    int readInt() {
+        f.read((char *) &integer, 4);
+        return integer;
+    }
+
+    float readFloat() {
+        f.read((char *) &floating, 4);
+        return floating;
+    }
+
+    ~BinReader() {
+        f.close();
     }
 };
 
-
-void shannonFanoTabela(vector<Znak>& znaki, int min, int max) {
-    int sum = 0;
-     int primerjava = 0;
-    int razlika1, razlika2;
-    int k, j;
-
-
-    if ((min + 1 == max) || (min == max) || (min > max)) {
-        if ((min == max) || (min > max)) {
-            return;
-        }
-        else {
-            znaki[min].codaZnaka.push_back(0);
-            znaki[max].codaZnaka.push_back(1);
+void fillVec(char tmp, vector<Element> &pairs) {
+    for (auto &pair: pairs) {
+        if (pair.character == tmp) {
+            pair.count++;
             return;
         }
     }
-    else {
-        for (int i = min; i < max; i++) {
-            sum += znaki[i].steviloPonovitev;
+    pairs.emplace_back(1, tmp);
+}
+
+unsigned int deli(vector<Element> &pairs, unsigned int dno, unsigned int vrh) {
+    unsigned int pe = pairs[dno].count;
+    unsigned int l = dno;
+    unsigned int d = vrh;
+    while (l < d) {
+        while (pairs[l].count <= pe && l < vrh)
+            l++;
+        while (pairs[d].count >= pe && d > dno)
+            d--;
+
+        if (l < d) {
+            swap(pairs[l], pairs[d]);
         }
-        sum += znaki[max].steviloPonovitev;
-        razlika1 = sum - primerjava;
-        if (razlika1 < 0) {
-            razlika1 *= -1;
-        }
+    }
+    swap(pairs[dno], pairs[d]);
+    return d;
+}
+
+void hitroUredi(vector<Element> &pairs, unsigned int dno, unsigned int vrh) {
+    if (dno < vrh) {
+        unsigned int j = deli(pairs, dno, vrh);
+        if (dno != j)
+            hitroUredi(pairs, dno, j - 1);
+        if (vrh != j)
+            hitroUredi(pairs, j + 1, vrh);
+    }
+}
+
+void sortPairs(vector<Element> &pairs) {
+    hitroUredi(pairs, 0, pairs.size() - 1);
+    reverse(pairs.begin(), pairs.end());
+}
+
+vector<Element> readFile(const string &path) {
+    BinReader br(path);
+    vector<Element> pairs;
+
+    while (br.f.peek() != EOF) {
+        fillVec(br.readByte(), pairs);
+    }
+
+    sortPairs(pairs);
+
+    return pairs;
+}
+
+void ShannonFano(vector<Element> &elements, int start, int end) {
+    unsigned int sum1 = 0, sum2 = 0;
+    int diff1, diff2;
+    int k, j;
+
+    //dodeli zadnjim elementom vrednosti
+    if ((start + 1) == end || start == end || start > end) {
+        if (start == end || start > end) return;
+        elements[end].codes.push_back(false);
+        elements[start].codes.push_back(true);
+        return;
+    } else {
+        for (int i = start; i < end; i++) sum1 += elements[i].count;
+        sum2 += elements[end].count;
+        diff1 = sum1 - sum2;
+        if (diff1 < 0) diff1 *= -1;
         j = 2;
-        while (j != max - min + 1) {
-            k = max - j;
-            sum = primerjava = 0;
-            for (int i = min; i <= k; ++i) {
-                sum += znaki[i].steviloPonovitev;
-            }
-            for (int i = max; i > k; --i) {
-                primerjava += znaki[i].steviloPonovitev;
-            }
-            razlika2 = sum - primerjava;
-            if (razlika2 < 0) {
-                razlika2 *= -1;
-            }
-            if (razlika2 >= razlika1) {
-                break;
-            }
-            razlika1 = razlika2;
+        while (j != end - start + 1) {
+            k = end - j;
+            sum1 = sum2 = 0;
+            for (int i = start; i <= k; ++i) sum1 += elements[i].count;
+            for (int i = end; i > k; --i) sum2 += elements[i].count;
+            diff2 = sum1 - sum2;
+            if (diff2 < 0) diff2 *= -1;
+            if (diff2 >= diff1) break;
+            diff1 = diff2;
             j++;
         }
         k++;
 
-        for (int i = min; i <= k; i++) {
-            znaki[i].codaZnaka.push_back(0);
-        }
-            
-        for (int i = k + 1; i <= max; i++) {
-            znaki[i].codaZnaka.push_back(1);
-        }
-            
-        shannonFanoTabela(znaki, min, k);
-        shannonFanoTabela(znaki, k + 1, max);
+        for (int i = start; i <= k; i++)
+            elements[i].codes.push_back(true);
+        for (int i = k + 1; i <= end; i++)
+            elements[i].codes.push_back(false);
+
+        //rekurzivni klic
+        ShannonFano(elements, start, k);
+        ShannonFano(elements, k + 1, end);
     }
 }
-    
-    
+
+void Encryption(const string &path) {
+    vector<Element> elements = readFile(path);
     /*
-    float sum=0;
-    float half = 0;
-    if ((min + 1 == max) || (min == max) || (min > max)) {
-        if ((min == max) || (min > max)) {
-            return;
-        }
-        else {
-            znaki[min].codaZnaka.push_back(0);
-            znaki[max].codaZnaka.push_back(1);
-            return;
-        }
+    for (auto &element: elements) {
+        cout << element.character;
     }
-    else {
-        for (int i = min;i <= max;i++) {
-            sum += znaki[i].steviloPonovitev;
-        }
-        half = sum / 2;
-        int temp = 0;
-        int i = min;
-        while (temp < half) {
-            temp = znaki[i].steviloPonovitev;
-            if (temp < half) {
-                ++i;
-            }
-        }
-        int temp2;
-        for (int j = i+1; j <= max;j++) {
-            temp2 = znaki[j].steviloPonovitev;
-        }
-        int razlika1 = temp - temp2;
-        if (razlika1 < 0) {
-            razlika1 *= -1;
-        }
-        int razlika2 = (temp - znaki[i].steviloPonovitev) - (temp2 + znaki[i].steviloPonovitev);
-        if (razlika2 < 0) {
-            razlika2 *= -1;
-        }
-
-        if (razlika1 > razlika2) {
-            i -= 1;
-        }
-
-        for (int k = min; k <= i;k++) {
-            znaki[k].codaZnaka.push_back(0);
-        }
-        for (int k = max;k > i;k--) {
-            znaki[k].codaZnaka.push_back(1);
-        }
-
-        shannonFanoTabela(znaki, min, i);
-        shannonFanoTabela(znaki, i + 1, max);
+    cout << endl;
+    for (auto &element: elements) {
+        cout << element.count;
     }
-}*/
+    */
+    //get tabela
+    ShannonFano(elements, 0, elements.size() - 1);
 
-
-
-void shannonFanoKodiranje(string path) {
+    /*
+    cout << endl;
+    for (auto &element: elements) {
+        for (auto &&code: element.codes) {
+            cout << code;
+        }
+        cout << endl;
+    }
+    */
 
     BinReader br(path);
+    BinWriter bw("encrypted.bin");
+    char tmp;
+    vector<bool> bits;
+    int bitsNum = 0;
+    int charNum = 0;
 
-    vector<unsigned char> vhodniNiz;
+    //table
 
-    vector<Znak>znaki(256);
-    int stetjeZnakov=0;
-
-    while (!br.f.eof()) {
-        vhodniNiz.push_back(br.readByte());
-        stetjeZnakov += 1;
-    }
-    br.f.close();
-
-    for (int i = 0; i < vhodniNiz.size();i++) {
-        znaki[vhodniNiz[i]].steviloPonovitev++;
-        //steviloPonovitev[vhodniNiz[i]]++;
-
-    }
-    br.f.close();
-
-    //urejanje
-    for (int i = 0;i < 256;i++) {
-        znaki[i].zacetniZnak = i;
-    }
-
-    BinWriter bw("test.bin");
-    bw.writeInt(znaki.size());
-    for (int i = 0;i < znaki.size();i++) {
-        bw.writeByte(znaki[i].zacetniZnak);
-        bw.writeInt(znaki[i].steviloPonovitev);
+    bw.writeInt(elements.size());
+    for (auto &element: elements) {
+        bw.writeByte(element.character);
+        bw.writeInt(element.count);
     }
 
 
+    //data encryption
 
-    int temp1, temp2;
-    for (int i = 0;i < znaki.size();i++)
-    {
-        for (int j = i + 1;j < znaki.size();j++) {
-            if (znaki[j].steviloPonovitev > znaki[i].steviloPonovitev) {
-                temp1 = znaki[i].steviloPonovitev;
-                znaki[i].steviloPonovitev = znaki[j].steviloPonovitev;
-                znaki[j].steviloPonovitev = temp1;
+    while (br.f.peek() != EOF) {
+        tmp = br.readByte();
+        charNum++;
 
-                temp2 = znaki[i].zacetniZnak;
-                znaki[i].zacetniZnak = znaki[j].zacetniZnak;
-                znaki[j].zacetniZnak = temp2;
-            }
-        }
-    }
-    
-
-    shannonFanoTabela(znaki, 0, 255);
-    int stetjeBitov = 0;
-    BinReader br2(path);
-    unsigned char prebraniZnak;
-    vector<int> bits;
-    
-   
-    while (br2.f.peek() != EOF) {
-        prebraniZnak = br2.readByte();
-        
-        for (int i = 0; i<znaki.size(); i++) {
-            if (znaki[i].zacetniZnak == prebraniZnak) {
-                bits = znaki[i].codaZnaka;
+        //find code of the read char
+        for (auto &element: elements) {
+            if (element.character == tmp) {
+                bits = element.codes;
                 break;
             }
         }
 
-        for (int i = 0;i < bits.size();i++) {
-            bw.writeBit(bits[i]);
-            stetjeBitov++;
+        //write bits
+        for (auto &&bit: bits) {
+            bw.writeBit(bit);
+            bitsNum++;
         }
     }
 
-    cout << "Kompresijsko razmerje: " << ((float)(stetjeZnakov * 8) / stetjeBitov);
+    //if left over bits
+    if (bw.getK() != 0) {
+        for (int i = bw.getK(); i <= 7; ++i) {
+            bw.writeBit(false);
+            bitsNum++;
+        }
+    }
+
+    cout << "Razmerje: " << (float) ((float) (charNum * 8) / (float) bitsNum);
 }
 
-int main(int argc, const char* argv[]) {
+void Decryption(const string &path) {
+    BinReader br(path);
+    BinWriter bw("decrypted.bin");
+
+    vector<Element> elements;
+    int count;
+    char character;
+    vector<bool> bits;
+
+    int charNum = br.readInt();
+    for (int i = 0; i < charNum; ++i) {
+        character = br.readByte();
+        count = br.readInt();
+        elements.emplace_back(count, character);
+    }
+
+    ShannonFano(elements, 0, elements.size() - 1);
+    /*
+    for (auto &element: elements) {
+        cout << element.character;
+    }
+    cout << endl;
+    for (auto &element: elements) {
+        cout << element.count;
+    }
+    cout << endl;
+    for (auto &element: elements) {
+        for (auto &&code: element.codes) {
+            cout << code;
+        }
+        cout << endl;
+    }
+    */
+    //br.f.ignore(1);
+    //cout<<(char)br.readByte();
+    br.readByte();
+    //bits.push_back(br.readBit());
+    while (br.f.good()) {
+        bits.push_back(br.readBit());
+        for (auto &element: elements) {
+            if (element.codes == bits) {
+                bw.writeByte(element.character);
+                bits.clear();
+            }
+        }
+    }
+}
 
 
+int main(int argc, const char *argv[]) {
+
+    //ce premalo argumentov
     if (argc < 2) {
         return -1;
     }
@@ -303,29 +325,13 @@ int main(int argc, const char* argv[]) {
     string path = argv[2];
     char option = argv[1][0];
 
-    if (option == 'c') shannonFanoKodiranje(path);
-    else if (option == 'd');
+    if (option == 'c') Encryption(path);
+    else if (option == 'd') Decryption(path);
 
     /*
-    BinWriter bw("test.bin");
-    bw.writeBit(1);
-    bw.writeBit(0);
-    bw.writeBit(0);
-    bw.writeBit(0);
-    bw.writeBit(0);
-    bw.writeBit(0);
-    bw.writeBit(1);
-    bw.writeBit(0);
-    bw.writeByte(bw.x);
-    bw.f.close();
-
-    BinReader br("test.bin");
-    br.readByte();
-    for (int i = 0; i < 8; ++i)
-        std::cout << (int)br.readBit() << "\n";
-    br.f.close();
+    Encryption("alice.txt");
+    Decryption("encrypted.bin");
     */
-    /*shannonFanoKodiranje("alice.txt");*/
 
     return 0;
 }
